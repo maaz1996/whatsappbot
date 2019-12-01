c508746b4a9a4c3eb520ae08fd7e1b678ca25103
Maaz Abdullah <maaz.abdullah111@gmail.com>
2019-12-02 01:01:39 +0530

    -setup complete for selenium in bot

---
+++ bot.py
@@ -0,0 +1,4 @@
+from selenium import webdriver
+# setup complete for selenium
+# to setup selenium with python in vscode visit https://code.visualstudio.com/docs/python/python-tutorial
+print("you are fucking good!")
