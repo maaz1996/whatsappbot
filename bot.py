544ea3f93e865d67fe31602ee8adb271f1c994d8
maaz1996 <maaz.abdullah111@gmail.com>
2019-12-02 11:08:17 +0530

    -changes in settings

--- bot.py
+++ bot.py
@@ -1,4 +1,5 @@
 from selenium import webdriver
 # setup complete for selenium
 # to setup selenium with python in vscode visit https://code.visualstudio.com/docs/python/python-tutorial
-print("you are fucking good!")
+#print("you are fucking good!")
+
