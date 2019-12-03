6917ec5fdcd51b3cd7d1d89294e82be72dd0efc6
Maaz Abdullah <maaz.abdullah111@gmail.com>
2019-12-04 02:25:37 +0530

    -error  in chromedriver; project proceed

--- bot.py
+++ bot.py
@@ -1,4 +1,8 @@
 from selenium import webdriver
 # setup complete for selenium
 # to setup selenium with python in vscode visit https://code.visualstudio.com/docs/python/python-tutorial
-print("you are fucking good!")
+#print("you are fucking good!")
+
+driver = webdriver.Chrome()
+driver.get('https://web.whatsapp.com/')
+print('reached the driver')
