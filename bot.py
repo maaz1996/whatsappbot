79b6afa9ec3751ae1bfd10a827e0da6c015cb404
Maaz Abdullah <maaz.abdullah111@gmail.com>
2019-12-05 02:19:14 +0530

    -added chrome driver ;success in driver

--- bot.py
+++ bot.py
@@ -3,6 +3,6 @@ from selenium import webdriver
 # to setup selenium with python in vscode visit https://code.visualstudio.com/docs/python/python-tutorial
 #print("you are fucking good!")
 
-driver = webdriver.Chrome()
+driver = webdriver.Chrome('F:\github\whatsappbot\chromedriver_win32\chromedriver.exe')
 driver.get('https://web.whatsapp.com/')
 print('reached the driver')
